806a41d5e424cdef45323a2ae13494b56f17fbb1
fgvlasov <fgvlasov@gmail.com>
2023-10-17 13:03:15 +0300

    new renaming

--- pages/api/unfavorite.ts
+++ pages/api/unfavorite.ts
@@ -1,31 +1,34 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import prismadb from '@/libs/prismadb';
+import prismadb from "@/libs/prismadb";
 
 import { getSession } from "next-auth/react";
 import { without } from "lodash";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   try {
-    if (req.method !== 'POST') {
+    if (req.method !== "POST") {
       return res.status(405).end();
     }
 
     const session = await getSession({ req });
 
     if (!session?.user?.email) {
-      throw new Error('Not signed in');
+      throw new Error("Not signed in");
     }
 
-    const { movieId } = req.body;
+    const { bookId } = req.body;
 
-    const existingMovie = await prismadb.movie.findUnique({
+    const existingBook = await prismadb.book.findUnique({
       where: {
-        id: movieId,
-      }
+        id: bookId,
+      },
     });
 
-    if (!existingMovie) {
-      throw new Error('Invalid ID');
+    if (!existingBook) {
+      throw new Error("Invalid ID");
     }
 
     const user = await prismadb.user.findUnique({
@@ -35,10 +38,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     });
 
     if (!user) {
-      throw new Error('Invalid email');
+      throw new Error("Invalid email");
     }
 
-    const updatedFavoriteIds = without(user.favoriteIds, movieId);
+    const updatedFavoriteIds = without(user.favoriteIds, bookId);
 
     const updatedUser = await prismadb.user.update({
       where: {
@@ -46,7 +49,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       },
       data: {
         favoriteIds: updatedFavoriteIds,
-      }
+      },
     });
 
     return res.status(200).json(updatedUser);
