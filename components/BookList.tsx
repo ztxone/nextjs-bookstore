9d001019998d3e0cf1c0cd543c1b697587abab72
fgvlasov <fgvlasov@gmail.com>
2023-10-24 21:34:30 +0300

    added Button

--- components/BookList.tsx
+++ components/BookList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { BookInterface } from "@/types";
 import BookCard from "@/components/BookCard";
@@ -10,8 +10,14 @@ interface BookListProps {
 }
 
 const BookList: React.FC<BookListProps> = ({ data, title }) => {
+  const [view, setView] = useState<"grid" | "table" | "twoColumns">("grid");
+
   if (isEmpty(data)) {
     return null;
+  }
+
+  function switchViews(selectedView: string): void {
+    setView(selectedView as "grid" | "table" | "twoColumns");
   }
 
   return (
@@ -20,7 +26,28 @@ const BookList: React.FC<BookListProps> = ({ data, title }) => {
         <p className="text-white text-md md:text-xl lg:text-2xl font-semibold mb-4">
           {title}
         </p>
-        <div className="grid grid-cols-4 gap-2">
+        <label htmlFor="viewSelect" className="text-white">
+          Select View:
+        </label>
+        <select
+          id="viewSelect"
+          className="bg-blue-500 text-white rounded-full px-4 py-2 ml-2 focus:outline-none focus:shadow-outline-blue"
+          value={view}
+          onChange={(e) => switchViews(e.target.value)}
+        >
+          <option value="grid">Grid</option>
+          <option value="table">Table</option>
+          <option value="twoColumns">Two Columns</option>
+        </select>
+        <div
+          className={
+            view === "table"
+              ? "table-view"
+              : view === "twoColumns"
+              ? "grid grid-cols-2 gap-2"
+              : "grid grid-cols-4 gap-2"
+          }
+        >
           {data.map((book) => (
             <BookCard key={book.id} data={book} />
           ))}
