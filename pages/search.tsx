1713835afd4d27230e5dd872c5d9e18743dc017c
fgvlasov <fgvlasov@gmail.com>
2023-11-02 15:56:10 +0200

    title

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,3 +1,4 @@
+import Layout from "@/components/Layout";
 import MultiPDFSearch from "@/components/Search/MultiPDFSearch";
 import useBookList from "@/hooks/useBookList";
 
@@ -5,8 +6,12 @@ const SearchPage: React.FC = () => {
   const { data: books = [] } = useBookList();
 
   const pdfUrls = books.map((book) => book.fileUrl);
-  console.log(pdfUrls);
-  return <MultiPDFSearch pdfUrls={pdfUrls} />;
+  //console.log(pdfUrls);
+  return (
+    <Layout>
+      <MultiPDFSearch pdfUrls={pdfUrls} />
+    </Layout>
+  );
 };
 
 export default SearchPage;
