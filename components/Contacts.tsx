7894e7983cefdb8bbc500dad059686deeea1be65
nayeem29 <nayeembracu29@gmail.com>
2023-11-02 18:12:09 +0200

    mailto link to team members

--- components/Contacts.tsx
+++ components/Contacts.tsx
@@ -5,8 +5,12 @@ const Contacts: React.FC<{}> = () => {
     <div className="px-4 md:px-12 py-4 space-y-8">
       <div>
         <p className="text-white text-md md:text-xl lg:text-2xl font-semibold mb-4">
-          <strong>Team:</strong> Awsaf uz zaman Ahmad, Khan Md Nayeem Hasan,
-          Mohajer Koohestani Ehsan, Fedor Vlasov
+          <strong>Team:</strong> 
+          <br />
+          <span className="cursor-pointer" onClick={(e) => {window.location.href ='mailto:ae9951@student.jamk.fi'}}>Awsaf uz zaman Ahmad</span><br/>
+          <span className="cursor-pointer" onClick={(e) => {window.location.href ='mailto:ad9348@student.jamk.fi'}}>Fedor Vlasov</span><br/>
+          <span className="cursor-pointer" onClick={(e) => {window.location.href ='mailto:ae9190@student.jamk.fi'}}>Mohajer Koohestani Ehsan</span><br/>
+          <span className="cursor-pointer" onClick={(e) => {window.location.href ='mailto:ae9200@student.jamk.fi'}}>Md Nayeem Hasan Khan</span><br/>
         </p>
         <p className="text-white text-md md:text-xl lg:text-2xl font-semibold mb-4">
           <strong>Course:</strong> Modern Software development - Frontend
